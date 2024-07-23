7ae08385bf65cec6eb73a494ff717c85960f7c8b
skiddedruck <106821194+skiddedruck@users.noreply.github.com>
2024-07-23 13:12:45 -0400

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -82,8 +82,8 @@ local vapeAssetTable = {
 	["vape/assets/RenderIcon.png"] = "rbxassetid://13350832775",
 	["vape/assets/VapeLogo1.png"] = "rbxassetid://13350860863",
 	["vape/assets/VapeLogo3.png"] = "rbxassetid://13350872035",
-	["vape/assets/VapeLogo2.png"] = "rbxassetid://13350876307",
-	["vape/assets/VapeLogo4.png"] = "rbxassetid://13350877564"
+	["vape/assets/VapeLogo2.png"] = "rbxassetid://18613891245",
+	["vape/assets/VapeLogo4.png"] = "rbxassetid://18613915740"
 }
 if inputService:GetPlatform() ~= Enum.Platform.Windows then
 	--mobile exploit fix
